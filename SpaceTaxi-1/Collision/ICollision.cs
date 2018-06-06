a7a05d808f937a75d081bcdc94541128a52e4b35
kam1986 <kam@live.dk>
2018-06-06 11:46:33 +0200

    remove

--- SpaceTaxi-1/Collision/ICollision.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpaceTaxi_1.Collision {
-    public interface ICollision {
-        /// <summary>
-        /// return bool to use collision match.
-        /// </summary>
-        /// <returns></returns>
-        bool CollidWith();
-    }
-}
